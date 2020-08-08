64f177b8dfcf8120767da3cb49aee406ddc3631f
Kyle Pollock <kyle.pollock7@gmail.com>
2020-08-08 01:37:14 -0500

    Add working but slow 11.6.5

--- 11.6.5.py
+++ 11.6.5.py
@@ -1,20 +1,21 @@
 import sys
 
 MAX_STICK_LENGTH = 1000
+memo = {}
 
 
-def minimum_cutting_cost(L, R, cut_positions, memo):
+def minimum_cutting_cost(L, R, cut_positions):
     if (L, R) in memo:
         return memo[(L, R)]
-    if len(cut_positions) == 0:
+    if not cut_positions:
         memo[(L, R)] = 0
         return 0
 
-    minimum_cost = MAX_STICK_LENGTH + 1
+    minimum_cost = sys.maxsize
     for i, cut_position in enumerate(cut_positions):
         cost = (R - L) + minimum_cutting_cost(
-            L, cut_position, cut_positions[:i], memo) + minimum_cutting_cost(
-                cut_position, R, cut_positions[i + 1:], memo)
+            L, cut_position, cut_positions[:i]) + minimum_cutting_cost(
+                cut_position, R, cut_positions[i + 1:])
 
         if cost < minimum_cost:
             minimum_cost = cost
@@ -30,6 +31,7 @@ for line in sys.stdin:
     num_cuts = int(sys.stdin.readline())
     cut_positions = [int(x) for x in sys.stdin.readline().split()]
 
+    memo = {}
     print(
-        f'The minimum cutting is {minimum_cutting_cost(0, stick_length, cut_positions, {})}.'
+        f'The minimum cutting is {minimum_cutting_cost(0, stick_length, cut_positions)}.'
     )
